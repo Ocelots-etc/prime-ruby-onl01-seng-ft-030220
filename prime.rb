8cb02952345b43ce94958ca88e7c642a9769ba40
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 18:35:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,23 @@
 numbers = [1..10]
 
-def prime?
-    return false if self <= 1
-    2.upto(Math.sqrt(self).to_i) do |x|
-        return false if self%x == 0
-    end
-    true
+def prime?(numbers)
+  is_prime = true
+  for i in 2..n-1
+    if n % 1 == 0
+      is_prime = false
+ numbers
 end
 
-def next_prime
-    n = self+1
-    n = n + 1 until n.prime?
-    n
-end
+#def prime?
+#    return false if self <= 1
+#    2.upto(Math.sqrt(self).to_i) do |x|
+#        return false if self%x == 0
+#    end
+#    true
+#end
 
-#def prime?(numbers)
-#  numbers
+#def next_prime
+#    n = self+1
+#    n = n + 1 until n.prime?
+#    n
 #end
