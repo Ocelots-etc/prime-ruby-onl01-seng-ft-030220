0f555bcbe8d9abc87a5fda3825d424f98253df68
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 18:42:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,31 +8,3 @@ def prime?(n)
         (2..n/2).none? { |i| n % i == 0}
     end
 end
-
-#def prime?(numbers)
-#  is_prime = true
-#  for i in 2..numbers-1
-#    if numbers % 1 == 0
-#      is_prime = false
-#    end
-#  end
-#end
- #=>> Failures:
-#  1) prime? returns true for prime numbers
-#     Failure/Error: expect(prime?(2)).to be(true)
-#       expected true
-#            got #<Range:17456260> => 2..1
-
-#def prime?
-#    return false if self <= 1
-#    2.upto(Math.sqrt(self).to_i) do |x|
-#        return false if self%x == 0
-#    end
-#    true
-#end
-
-#def next_prime
-#    n = self+1
-#    n = n + 1 until n.prime?
-#    n
-#end
